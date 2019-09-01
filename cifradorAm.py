16055cf122c8d524a055f7706d883e8e2439c1f2
ocastilloc <oscar.rodrigo.castillo@gmail.com>
2019-09-01 00:18:33 -0400

    Actualizar cifradorAm.py
    Primera actualización.

--- cifradorAm.py
+++ cifradorAm.py
@@ -1,1 +1,9 @@
+import euclidesExtendido
 
+def main():
+    
+    #Obtener el máximo común divisor para comprobar que n e x sean coprimos (solo divisibles por 1)
+    #Tener el inverso multiplicativo para la función descifrar.
+    diccionario = euclidesExtendido.euclidesExtendido(29,5)
+    
+    return diccionario
