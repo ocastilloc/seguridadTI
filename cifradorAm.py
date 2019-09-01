a8775390c758fca34dc51e8ef3ac3c8251b51643
ocastilloc <oscar.rodrigo.castillo@gmail.com>
2019-09-01 00:31:37 -0400

    Actualizar cifradorAm
    Quitar espacios en blanco de la frase.

--- cifradorAm.py
+++ cifradorAm.py
@@ -1,9 +1,13 @@
 import euclidesExtendido
 
-def main():
+#palabra: corresponde a un texto a cifrar o descifrar
+#modo: para cifrar utilizar la palabra "cifrar" y para descifrar "descifrar"
+def main(palabra,modo):
+    #eliminar espacios en blanco
+    palabraFormateada =palabra.replace(" ", "")
     
     #Obtener el máximo común divisor para comprobar que n e x sean coprimos (solo divisibles por 1)
     #Tener el inverso multiplicativo para la función descifrar.
     diccionario = euclidesExtendido.euclidesExtendido(29,5)
     
-    return diccionario
+    return diccionario, palabraFormateada
