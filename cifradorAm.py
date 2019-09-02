6af974c9c7f7982b9f2f96438f8245d9602c3b3d
ocastilloc <oscar.rodrigo.castillo@gmail.com>
2019-09-01 23:20:41 -0400

    Actualización
    Versión 1.0 de la aplicación cifrar/descifrar

--- cifradorAm.py
+++ cifradorAm.py
@@ -8,11 +8,12 @@ import descifrar
 def main(palabra,modo):
     #eliminar espacios en blanco
     palabraFormateada =palabra.replace(" ", "").upper()
+    modoOperar = modo.upper()
     
     #diccionario con el alfabeto español
     alfabeto={"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,
               "K":10,"L":11,"M":12,"N":13,"Ñ":14,"O":15,"P":16,"Q":17,"R":18,
-              "S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26}
+              "S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26,"#":27,".":28}
     
     #constanteDecimacion y constanteDesplazamiento serán obtenidas
     #de la funcion f(clave)=x,y
@@ -32,9 +33,12 @@ def main(palabra,modo):
         sys.exit('La constanteDecimacion y modulo no son coprimos')
     
     #Modo de operacion cifrar o descifrar
-    if modo == "cifrar":
+    
+    if modoOperar == "CIFRAR":
         palabraResultado = cifrar.cifrar(alfabeto,palabraFormateada,constanteDecimacion,constanteDesplazamiento,modulo)
-    else:
+    elif modoOperar == "DESCIFRAR":
         palabraResultado = descifrar.descifrar(alfabeto,diccionario,palabraFormateada,constanteDecimacion,constanteDesplazamiento,modulo)
+    else:
+         sys.exit('No existe el modo de operación ingresado')
 
     return palabraResultado
--- cifrar.py
+++ cifrar.py
@@ -2,17 +2,23 @@ def cifrar(alfabeto,palabraFormateada,constanteDecimacion,constanteDesplazamient
     
      #Obtener el codigo de cada letra para cifrar
     largoPalabra=len(palabraFormateada)
+    largoAlfabeto=len(alfabeto)-1;
+    
     palabraCifrada = ""
     
     for contador in range(largoPalabra):
         letraMensaje = palabraFormateada[contador]
         codigoLetra = alfabeto[letraMensaje]
-      
+     
         #Formula cifrar:
         #C= (X*A+Y) MOD N
+        valorLetraCifrada=0
         valorLetraCifrada = (constanteDecimacion * codigoLetra + constanteDesplazamiento) % modulo
         
-        letraCifrada = list(alfabeto.keys())[list(alfabeto.values()).index(valorLetraCifrada)]
+        if valorLetraCifrada > largoAlfabeto:
+            letraCifrada = list(alfabeto.keys())[list(alfabeto.values()).index(largoAlfabeto)]
+        else:
+            letraCifrada = list(alfabeto.keys())[list(alfabeto.values()).index(valorLetraCifrada)]
         
         palabraCifrada = palabraCifrada + letraCifrada
     
