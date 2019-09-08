f3a9393671ea1f5d752ca7a59359ad6335a06652
ocastilloc <oscar.rodrigo.castillo@gmail.com>
2019-09-08 02:23:02 -0300

    Logica de clave
    Primera versón de prueba de la logica de la clave.

--- cifrar.py
+++ cifrar.py
@@ -1,3 +1,4 @@
+import clave
 def cifrar(alfabeto,palabraFormateada,constanteDecimacion,constanteDesplazamiento,modulo):
     
      #Obtener el codigo de cada letra para cifrar
@@ -6,10 +7,32 @@ def cifrar(alfabeto,palabraFormateada,constanteDecimacion,constanteDesplazamient
     
     palabraCifrada = ""
     
+    valX=0
+    valY=1
+    x=0
+    y=0
+    
     for contador in range(largoPalabra):
+        
+        if x == 0:
+            x=valX
+            y=valY
+         
+        contenidoClave = clave.diccionarioClave()
+        largoClave = len(contenidoClave)-1
+        
+        if x >= largoClave:
+            x=0
+            y=1
+        
+        clave.clave(x,y,contenidoClave)
+        x=x+1
+        y=x+1
+    
+        
         letraMensaje = palabraFormateada[contador]
         codigoLetra = alfabeto[letraMensaje]
-     
+        
         #Formula cifrar:
         #C= (X*A+Y) MOD N
         valorLetraCifrada=0
--- clave.py
+++ clave.py
@@ -1,1 +1,20 @@
+# Contiene una clave o grupo de ellas para generar la cosntante de decimación y desplazamiento.
+def clave(x,y,clave):
+    
+    letraActual = clave[x];
+    letraSiguiente = clave[y]
+    
+    print("Decimacion ",letraActual)
+    print("Desplazamiento",letraSiguiente)
+    
+    resultado=""
+    resultado= {"decimacion":letraActual, "desplazamiento":letraSiguiente}
+    
+    return resultado
 
+#funcion que contiene una o más claves privadas
+def diccionarioClave():
+     
+    claves = "XBCDFEWI"
+     
+    return claves
