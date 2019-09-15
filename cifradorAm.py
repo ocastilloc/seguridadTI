cd4ad25c635689b94e4bfff5f18c2c563bdb89b6
ocastilloc <oscar.rodrigo.castillo@gmail.com>
2019-09-15 01:38:27 -0300

    Actualización
    Manejo de errores y primera versión lectura y escritura archivos.

--- cifradorAm.py
+++ cifradorAm.py
@@ -1,29 +1,76 @@
-import sys
+import sys, os
 import cifrar
 import descifrar
+import re
+from unicodedata import normalize
 
 #palabra: corresponde a un texto a cifrar o descifrar
 #modo: para cifrar utilizar la palabra "cifrar" y para descifrar "descifrar"
-def main(palabra,modo):
-    #eliminar espacios en blanco
-    #palabraFormateada =palabra.replace(" ", "").upper()
-    palabraFormateada = palabra.upper().strip()
-    modoOperar = modo.upper()
+def main(modo):
     
-    #diccionario con el alfabeto español
-    alfabeto={"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,
-              "K":10,"L":11,"M":12,"N":13,"Ñ":14,"O":15,"P":16,"Q":17,"R":18,
-              "S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26," ":27,".":28}
+    contenidoArchivo=""
+    leerArchivoEntrada=open('entrada.txt', 'r')
+    palabraResultado=""
+    leerArchivoSalida=open("salida.txt", "w")
     
-    modulo=29 # n
+    try:
+        
+        #Instrucciones del aplicativo
+        #Abrir texto y lerlo
+        contenidoArchivo+=leerArchivoEntrada.read()
+        
+        #Eliminar diacriticos
+        contenidoArchivo = re.sub(
+            r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1", normalize( "NFD", contenidoArchivo), 0, re.I
+        )
     
-    #Modo de operacion cifrar o descifrar
+        #Normalizar NFC
+        contenidoArchivo = normalize( 'NFC', contenidoArchivo)
+   
+        #eliminar espacios en blanco
+        contenidoArchivo = contenidoArchivo.upper().strip().replace(",","")
+        palabraFormateada = contenidoArchivo.upper().strip().replace("\n"," ")
+        
+        lista = list(palabraFormateada)
+        modoOperar = modo.upper()
+        
+        #diccionario con el alfabeto español
+        alfabeto={"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6,"H":7,"I":8,"J":9,
+                  "K":10,"L":11,"M":12,"N":13,"Ñ":14,"O":15,"P":16,"Q":17,"R":18,
+                  "S":19,"T":20,"U":21,"V":22,"W":23,"X":24,"Y":25,"Z":26," ":27,".":28}
+        
+        modulo=29 # n
+        
+        #Modo de operacion cifrar o descifrar
+        
+        if modoOperar == "CIFRAR":
+            palabraResultado = cifrar.cifrar(alfabeto,lista,modulo)
+        elif modoOperar == "DESCIFRAR":
+            palabraResultado = descifrar.descifrar(alfabeto,lista,modulo)
+        else:
+             sys.exit('No existe el modo de operación ingresado')
+        
+        #escribir archivo
+        print(palabraResultado, file=leerArchivoSalida)
+        
+        #Instrucciones del error   
+    except BlockingIOError as eb:
+        print("Problema con el archivo: ",eb)
+    except FileNotFoundError as ef:
+        print("Archivo no encontrado: ",ef)
+    except Exception as e:
+        print("Error general: ",e)
+        tipoException, exc_obj, exc_tb = sys.exc_info()
+        archivoError = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        print("Tipo de excepción : ",tipoException)
+        print("Archivo que contiene el error : ",archivoError)
+        print("Número de la linea con el error : ",exc_tb.tb_lineno)
+        
+    finally:
+        #cerrar recursos
+         leerArchivoEntrada.close()
+         leerArchivoSalida.close()
     
-    if modoOperar == "CIFRAR":
-        palabraResultado = cifrar.cifrar(alfabeto,palabraFormateada,modulo)
-    elif modoOperar == "DESCIFRAR":
-        palabraResultado = descifrar.descifrar(alfabeto,palabraFormateada,modulo)
-    else:
-         sys.exit('No existe el modo de operación ingresado')
-
+    #Leer archivo de entrada desde dirección web:
+    
     return palabraResultado
--- cifrar.py
+++ cifrar.py
@@ -1,60 +1,72 @@
 import clave
+import sys, os
 
 def cifrar(alfabeto,palabraFormateada,modulo):
     
-     #Obtener el codigo de cada letra para cifrar
-    largoPalabra=len(palabraFormateada)
-    largoAlfabeto=len(alfabeto)-1;
+    try:
+        
+         #Obtener el codigo de cada letra para cifrar
+         largoPalabra=len(palabraFormateada)
+         largoAlfabeto=len(alfabeto)-1;
     
-    palabraCifrada = ""
+         palabraCifrada = ""
+            
+         valX=0
+         valY=1
+         x=0
+         y=0
+         indiceClave=0
+            
+         for contador in range(largoPalabra):
+                
+             if x == 0:
+                 x=valX
+                 y=valY
+                      
+             llamarContenidoClave = clave.diccionarioClave(indiceClave)
+             contenidoClave = llamarContenidoClave["clave"]
+                
+             largoClave = len(contenidoClave)-1
+             cantidadClave = llamarContenidoClave["cantidadClaves"] -1
+                
+             if indiceClave >= cantidadClave:
+                 indiceClave = 0
+                
+             if x >= largoClave:
+                 x=0
+                 y=1
+                 indiceClave = indiceClave+1
+                
+             resultadoClave = clave.clave(x,y,contenidoClave,alfabeto)
+             x=x+1
+             y=x+1
+                
+             valorDecimacion = resultadoClave["valorDecimacion"]
+             valorDesplazamiento = resultadoClave["valorDesplazamiento"]
+            
+                
+             letraMensaje = palabraFormateada[contador]
+             codigoLetra = alfabeto[letraMensaje]
+                
+             #Formula cifrar:
+             #C= (X*A+Y) MOD N
+             valorLetraCifrada=0
+             valorLetraCifrada = (valorDecimacion * codigoLetra + valorDesplazamiento) % modulo
+                
+             if valorLetraCifrada > largoAlfabeto:
+                 letraCifrada = list(alfabeto.keys())[list(alfabeto.values()).index(largoAlfabeto)]
+             else:
+                 letraCifrada = list(alfabeto.keys())[list(alfabeto.values()).index(valorLetraCifrada)]
+                
+             palabraCifrada = palabraCifrada + letraCifrada
     
-    valX=0
-    valY=1
-    x=0
-    y=0
-    indiceClave=0
+    except Exception as e:
+         print("Error general: ",e)
+         tipoException, exc_obj, exc_tb = sys.exc_info()
+         archivoError = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+         print("Tipo de excepción : ",tipoException)
+         print("Archivo que contiene el error : ",archivoError)
+         print("Número de la linea con el error : ",exc_tb.tb_lineno)
     
-    for contador in range(largoPalabra):
-        
-        if x == 0:
-            x=valX
-            y=valY
-         
-        llamarContenidoClave = clave.diccionarioClave(indiceClave)
-        contenidoClave = llamarContenidoClave["clave"]
-        
-        largoClave = len(contenidoClave)-1
-        cantidadClave = llamarContenidoClave["cantidadClaves"] -1
-        
-        if indiceClave >= cantidadClave:
-            indiceClave = 0
-        
-        if x >= largoClave:
-            x=0
-            y=1
-            indiceClave = indiceClave+1
-        
-        resultadoClave = clave.clave(x,y,contenidoClave,alfabeto)
-        x=x+1
-        y=x+1
-        
-        valorDecimacion = resultadoClave["valorDecimacion"]
-        valorDesplazamiento = resultadoClave["valorDesplazamiento"]
-    
-        
-        letraMensaje = palabraFormateada[contador]
-        codigoLetra = alfabeto[letraMensaje]
-        
-        #Formula cifrar:
-        #C= (X*A+Y) MOD N
-        valorLetraCifrada=0
-        valorLetraCifrada = (valorDecimacion * codigoLetra + valorDesplazamiento) % modulo
-        
-        if valorLetraCifrada > largoAlfabeto:
-            letraCifrada = list(alfabeto.keys())[list(alfabeto.values()).index(largoAlfabeto)]
-        else:
-            letraCifrada = list(alfabeto.keys())[list(alfabeto.values()).index(valorLetraCifrada)]
-        
-        palabraCifrada = palabraCifrada + letraCifrada
     
     return palabraCifrada
