5928707a662ae8a3c872277ac3bdede14cd3d4e1
ocastilloc <oscar.rodrigo.castillo@gmail.com>
2019-09-19 16:44:08 -0300

    Agregar comentarios
    Agregar comentarios en la función euclidesExtendido.

--- euclidesExtendido.py
+++ euclidesExtendido.py
@@ -1,10 +1,15 @@
+#El modulo os nos permite acceder a funcionalidades dependientes del Sistema Operativo, por ejemplo
+#manipular estructura del sistema de archivos (Leer y escribir).
+#El modulo sys es el encargado de proveer variables y funcionalidades, directamente relacionadas con el intérprete.
 import sys, os
-
+#Funcion utilizada para obtener el maximo comun divisor y el inverso multiplicativo, utilizado en la funcion
+#para descifrar.
+#a: corresponde al valor de decimacion
+#b: corresponde al valor del modulo
 def euclidesExtendido(a, b):
     
 	#En el caso de que b tenga el valor 0, retornar 0,1,0
 	# no puede existir un dividor 0
-    
     try:
         if b == 0:
             return 0,1,0
@@ -26,12 +31,14 @@ def euclidesExtendido(a, b):
             #Actualizar valores en cada iteración
             u0 = u1; v0 = v1
             u1 = u; v1 = v
-            
+            #La duncion retorna un diccionario con tres valores:
+            #Maximo comun divisor, el minimo y el inverso multiplicativo.
             diccionario={"maximoComunDivisor":a,"minimo":u0,"inversoMultiplicativo":v0}
         
     except Exception as e:
         tipoException, exc_obj, exc_tb = sys.exc_info()
         archivoError = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        #En el caso de error la excepción sera lanzada al programa que invoca a la función euclidesExtendido.
         raise ValueError("Error en archivo : ",archivoError," en la linea : ",exc_tb.tb_lineno) from e  
  
     return diccionario
