24b98a4a622dd542eba64db4c653098ed9a09384
ocastilloc <oscar.rodrigo.castillo@gmail.com>
2019-09-20 01:48:18 -0300

    Documentación modulo clave.py
    Documentación modulo clave.py

--- clave.py
+++ clave.py
@@ -1,6 +1,12 @@
-# Contiene una clave o grupo de ellas para generar la cosntante de decimaciÃ³n y desplazamiento.
+#El modulo os nos permite acceder a funcionalidades dependientes del Sistema Operativo, por ejemplo
+#manipular estructura del sistema de archivos (Leer y escribir).
+#El modulo sys es el encargado de proveer variables y funcionalidades, directamente relacionadas con el intérprete.
 import sys, os
-
+#Funcion utilizada para obtener el valor decimacion y desplazamiento.
+#x: indice actual del ciclo
+#y: indice siguiente del ciclo
+#clave: corresponde a una clave privada
+#alfabeto: lista con el alfabeto español.
 def clave(x,y,clave,alfabeto):
     
     try:
@@ -11,29 +17,34 @@ def clave(x,y,clave,alfabeto):
         valorDesplazamiento = alfabeto[letraMensajeDesplazamiento]
     
         resultado=""
+        #Lista con el retorno de la función, valor de decimacion y desplazamiento.
         resultado= {"valorDecimacion":valorDecimacion, "valorDesplazamiento":valorDesplazamiento}
     
     except Exception as e:
          tipoException, exc_obj, exc_tb = sys.exc_info()
          archivoError = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-         raise ValueError("Error en archivo : ",archivoError," en la linea : ",exc_tb.tb_lineno) from e
-     
+         #Lanzar la excepcion al modulo que invoca la funcion.
+         raise ValueError("Error en archivo : ",archivoError," en la linea : ",exc_tb.tb_lineno) from e    
 
     return resultado
 
-#funcion que contiene una o mÃ¡s claves privadas
+#funcion que contiene una o mas claves privadas
 def diccionarioClave(indiceClave):
     
     try:
+        #Lista con 8 claves privadas
         diccionarioClave = {'clave': ['XHCDFEWI','ZFHJVÃ‘TS','OJGNRQPM','HRLÃ‘GVW','SXODUZER','ICQYG.Ã‘T','EWLIFSZO','RKNUVKXL'] }
+        #corresponde a una clave privada
         clave = diccionarioClave['clave'][indiceClave]
+        #valor numerico con el total de claves privadas
         cantidadClave = len(diccionarioClave['clave'])
-    
+        #retorno de la funcion, una clave privada y cantidad de claves
         retorno = {"clave":clave,"cantidadClaves":cantidadClave}
     
     except Exception as e:
         tipoException, exc_obj, exc_tb = sys.exc_info()
         archivoError = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+         #Lanzar la excepcion al modulo que invoca la funcion.
         raise ValueError("Error en archivo : ",archivoError," en la linea : ",exc_tb.tb_lineno) from e
      
     return retorno
